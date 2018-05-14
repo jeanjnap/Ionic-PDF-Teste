7c7b2bda1ad0fb8025b024c8098ae1a89d699190
JeanJNAP <jeanjnap@gmail.com>
2018-05-14 20:27:32 -0300

    Add library

--- testePdf2/src/pages/home/home.ts
+++ testePdf2/src/pages/home/home.ts
@@ -7,6 +7,8 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  pdfSrc: string = 'http://www.camisetaexcelencia.com.br/testes/PDF/PDF%201.pdf';
+
   constructor(public navCtrl: NavController) {
 
   }
